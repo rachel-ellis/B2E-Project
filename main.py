5b2e071e3f87ffe4a90feb7450154f0924b1b4ed
rachel-ellis <rjellis@ualberta.ca>
2022-08-09 10:37:30 -0600

    add and subtract

--- main.py
+++ main.py
@@ -4,9 +4,46 @@ multiple
 line
 '''
 
-# this function takes 2 arguments and adds them (should be numbers)
-def add(a, b):
-    return a+b
+
+def isNum(num):
+    return type(num) == int or type(num) == float
+
+# this function takes 1 argument which is a list of numbers and adds them
+def add(a):
+    result = 0
+    for num in a:
+        if isNum(num):
+            result += num
+        else:
+            raise Exception("Error: not a number")
+    return result
+
+# this function takes 1 argument which is a list of numbers and subtracts them
+def subtract(a):
+    count = 0
+    result = 0
+    for num in a:
+        # first time in the loop
+        if count == 0:
+            result = num
+            count += 1
+            continue
+        if isNum(num):
+            result -= num
+        else:
+            raise Exception("Error: not a number")
+        count += 1
+    return result
 
 # main part of the program
-print(add(1, 2))
+try:
+    myList = map(float, input("Enter a list of numbers to add/subtract: ").split())
+    choice = int(input("Enter 1 to add, 2 to subtract: "))
+    if choice == 1:
+        print(add(myList))
+    elif choice == 2:
+        print(subtract(myList))
+    else:
+        print("How did I get here")
+except:
+    print("An error occurred")
