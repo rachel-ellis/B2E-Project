41f580d2e08b062ecc2e5fd48278ec977af00854
rachel-ellis <rjellis@ualberta.ca>
2022-08-09 09:50:41 -0600

    this is a commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+this 
+multiple
+line
+'''
+
+# this function takes 2 arguments and adds them (should be numbers)
+def add(a, b):
+    return a+b
+
+# main part of the program
+print(add(1, 2))
